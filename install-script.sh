269af9f5f4c086eb68d27e94ccd85439da8b385a
Jay Clifford <jclifford@influxdata.com>
2022-10-27 12:19:25 +0100

    added docker support

---
+++ install-script.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Installing docker
+sudo apt-get update && sudo apt-get upgrade -y
+curl -sSL https://get.docker.com | sh
+sudo usermod -aG docker ${USER}
+
+#Installing docker-compose
+sudo apt-get install -y libffi-dev libssl-dev
+sudo pip3 install docker-compose
+sudo systemctl enable docker
+
+# influxdb.key GPG Fingerprint: 05CE15085FC09D18E99EFB22684A14CF2582E0C5
+wget -q https://repos.influxdata.com/influxdb.key
+echo '23a1c8836f0afc5ed24e0486339d7cc8f6790b83886c4c96995b88a061c5bb5d influxdb.key' | sha256sum -c && cat influxdb.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdb.gpg > /dev/null
+echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdb.gpg] https://repos.influxdata.com/debian stable main' | sudo tee /etc/apt/sources.list.d/influxdata.list
+
+sudo apt-get update && sudo apt-get install influxdb2-cli
+
+influx config create \
+  --config-name demo \
+  --host-url http://localhost:8086 \
+  --org influxdata \
+  --token edge \
+  --active
